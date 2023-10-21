f67f2d5963689b16868183a7950ce52d8d7fd0c6
Disha Agarwal <68860743+dishamagarwal@users.noreply.github.com>
2023-10-21 13:39:23 -0400

    spring initilizer

---
+++ src/test/java/com/spotify/synesthesia/SynesthesiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotify.synesthesia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynesthesiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

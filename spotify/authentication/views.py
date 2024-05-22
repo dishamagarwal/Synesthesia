0851aa0b3144f1f99cbd2507a11d7b449534fd8a
Disha Agarwal <68860743+dishamagarwal@users.noreply.github.com>
2024-05-22 14:20:56 +0530

    mapping color to music

--- spotify/authentication/views.py
+++ spotify/authentication/views.py
@@ -13,8 +13,7 @@ def login(request):
     scope = "user-library-read playlist-modify-private"
     redirect_uri = "http://127.0.0.1:8080/home"
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, redirect_uri=redirect_uri))
-    results = sp.current_user_saved_tracks()
-    print("Successully logged in", sp)
+    # results = sp.current_user_saved_tracks()
     return redirect(reverse('home'))
 
 def home(request):
@@ -28,7 +27,6 @@ def generatePlaylistName():
     else: 
         words = ["Cool", "Tunes"]
     playlist_name = f"{words[0].capitalize()} {words[1].capitalize()}"
-    print(f"Generated playlist name: {playlist_name}")
     return playlist_name
 
 def getPlaylist(request):
@@ -36,37 +34,43 @@ def getPlaylist(request):
     try:
         access_token = request.session.get('spotify_token')
         sliders = json.loads(request.body).get('sliders', {})
+        # print("sliders", sliders)
 
         recommendations = sp.recommendations(
-            seed_genres = sp.recommendation_genre_seeds()['genres'][:5], # send the actual genres you want
+            seed_genres = sp.recommendation_genre_seeds()['genres'][:5], # ToDO: filter genres based on color
             limit = 13,
-            target_valence = sliders['melancholic'],  # mapping melancholic to valence
-            target_danceability = sliders['electronic'],  # mapping electronic to danceability
-            target_energy = sliders['energy']
+            target_acousticness = sliders.get("neutral", 0.5),  # mapping neutral to acousticness
+            target_danceability = sliders.get("darkness", 0.5),  # mapping darkness to danceability
+            max_duration_ms = 300000,
+            target_energy = sliders.get("darkness", 0.5),  # mapping darkness to energy
+            # target_instrumentalness = sliders.get("", 0.5),  # mapping melancholic to instrumentalness
+            target_liveness = sliders.get("saturation", 0.5),  # mapping saturation to liveness
+            target_loudness = sliders.get("contrast", 0.5),  # mapping contrast to loudness
+            # target_popularity = sliders.get("", 0.5),  # calculte popularity based on hex
+            target_speechiness = sliders.get("texture", 0.5),  # mapping texture to speechiness
+            target_tempo = sliders.get("saturation", 0.5),  # mapping saturation to tempo
+            target_valence = sliders.get("darkness", 0.5)  # mapping darkness to valence
         )
         tracks = []
         for track in recommendations["tracks"]:
             track_info = {
                 "name": track["name"],
                 "artists": [artist["name"] for artist in track["artists"]],
-                "album": track["album"]["name"],
+                # "album": track["album"]["name"],
                 # "preview_url": track["preview_url"]
             }
             tracks.append(track_info)
+        print(tracks)
 
         # Generate a unique and cool name for the playlist (e.g., "My Awesome Playlist")
         playlist_name = generatePlaylistName()
-        # # Create a new playlist for the user
-        # user_id = sp.me()['id']  # Get the user's Spotify ID
-        # playlist = sp.user_playlist_create(user=user_id, name=playlist_name, public=False)
-        # # Extract the playlist ID from the response
-        # playlist_id = playlist['id']
-        # # Get the track URIs from the recommendations
-        # track_uris = [track['uri'] for track in recommendations['tracks']]
-        # # Add tracks to the created playlist
-        # sp.playlist_add_items(playlist_id=playlist_id, items=track_uris)
-        print(f"Playlist '{playlist_name}' created and tracks added successfully!")
-        
+        user_id = sp.me()['id'] 
+        playlist = sp.user_playlist_create(user=user_id, name=playlist_name, public=False)
+        playlist_id = playlist['id']
+        track_uris = [track['uri'] for track in recommendations['tracks']]
+        sp.playlist_add_items(playlist_id=playlist_id, items=track_uris)
+        # print(f"Playlist '{playlist_name}' created and tracks added successfully!")
+
         return JsonResponse({'message': 'got data!'})
     except json.JSONDecodeError as e:
         print('Error decoding JSON:', str(e))  # Print JSON decode error for debugging
